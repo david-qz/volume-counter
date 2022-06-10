201e430c92573d2bbeed92157815808380e710cc
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-10 12:29:24 -0700

    Implement volume counter

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+// State
+let volume = 10;
 
-// state
+// Helper functions
+function clamp(value, min, max) {
+    return Math.min(max, Math.max(min, value));
+}
+
+// Counter component
+const counterRoot = document.getElementById('volume-counter');
+const [downButton, upButton] = counterRoot.querySelectorAll('button');
+const volumeDisplay = counterRoot.querySelector('span');
+
+function displayCounter() {
+    volumeDisplay.textContent = volume;
+}
+
+function getChangeVolumeCallback(delta) {
+    return () => {
+        volume = clamp(volume + delta, 1, 20);
+        displayCounter();
+    };
+}
+
+downButton.addEventListener('click', getChangeVolumeCallback(-1));
+upButton.addEventListener('click', getChangeVolumeCallback(1));
 
 
-// component
-    // define and grab DOM elements
-    // display functions
-    // subscribe to events
-        // event handlers - what needs to happen?
-        // logic and calculations
-        // state update
-        // re-display components
-
-
-// page load actions
+// Page load actions
+displayCounter();
